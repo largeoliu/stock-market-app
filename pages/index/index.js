c0ff0ebcf03adb3ccb2aa7ec6d95af6a924fd719
liuhao <largeoliu@163.com>
2025-07-29 23:58:13 +0800

    refactor(pages): 移除不必要的代码和功能
    - 删除了 detail 页面中的数据说明部分
    - 删除了 index 页面中的热门股票和功能介绍部分
    - 移除了相关的 CSS 样式
    - 简化了 index 页面的加载逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,73 +4,16 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    hotStocks: [], // 热门股票
     recentSearches: [], // 最近搜索
-    loading: true
+    loading: false
   },
 
   onLoad() {
-    this.loadHotStocks()
     this.loadRecentSearches()
   },
 
   onShow() {
     this.loadRecentSearches()
-  },
-
-  // 加载热门股票
-  async loadHotStocks() {
-    try {
-      util.showLoading('加载中...')
-      
-      // 模拟热门股票数据
-      const hotStocks = [
-        {
-          symbol: '000001.SZ',
-          name: '平安银行',
-          market: 'A股',
-          price: 12.50,
-          change: 0.15,
-          changePercent: 1.22
-        },
-        {
-          symbol: '00700.HK',
-          name: '腾讯控股',
-          market: '港股',
-          price: 368.20,
-          change: -5.80,
-          changePercent: -1.55
-        },
-        {
-          symbol: 'AAPL',
-          name: '苹果公司',
-          market: '美股',
-          price: 175.43,
-          change: 2.15,
-          changePercent: 1.24
-        },
-        {
-          symbol: '000002.SZ',
-          name: '万科A',
-          market: 'A股',
-          price: 8.95,
-          change: -0.12,
-          changePercent: -1.32
-        }
-      ]
-      
-      this.setData({
-        hotStocks: hotStocks,
-        loading: false
-      })
-      
-      util.hideLoading()
-    } catch (error) {
-      console.error('加载热门股票失败:', error)
-      util.hideLoading()
-      util.showToast('加载失败，请重试')
-      this.setData({ loading: false })
-    }
   },
 
   // 加载最近搜索
