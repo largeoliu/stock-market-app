5593103532f88a5f7f7a0a96532aed2c7393e765
liuhao <largeoliu@163.com>
2025-07-31 18:39:39 +0800

    build(deploy): 更新版本号至 1.0.6
    - 将版本号从 1.0.5 修改为 1.0.6
    - 更新最后上传时间至 2025-07-31T10:39:27.865Z
    - 保持其他配置项不变

--- deploy.config.js
+++ deploy.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "version": "1.0.5",
+  "version": "1.0.6",
   "desc": "添加用户行为埋点功能",
   "outputPath": "./dist",
   "ignoreFiles": [
@@ -31,6 +31,6 @@ module.exports = {
     "beforeDeploy": [],
     "afterDeploy": []
   },
-  "lastUpload": "2025-07-31T06:48:04.096Z",
+  "lastUpload": "2025-07-31T10:39:27.865Z",
   "deployHistory": []
 };
